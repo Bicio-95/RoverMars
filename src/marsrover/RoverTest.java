068a20d41b5315502e1c3241e3960b691c0cdc6a
Beretta <Beretta@Beretta-PC.mshome.net>
2017-03-26 19:47:39 -0300

    COM TESTE

--- src/marsrover/RoverTest.java
+++ src/marsrover/RoverTest.java
@@ -1,55 +1,37 @@
-/**
- * 
- */
 package marsrover;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.math.BigDecimal;
+
+import org.junit.Before;
 import org.junit.Test;
 
 import junit.framework.TestCase;
 
-/**
- * @author abhinaba
- *
- */
-public class RoverTest extends TestCase {
-	public static final Integer N = 1;
-	public static final Integer E = 2;
-	public static final Integer S = 3;
-	public static final Integer W = 4;
-	Integer x = 0;
-	Integer y = 0;
-	Integer facing = N;
-	/*public void testRoverMovementWithInputOne(){
-		final String roverCurrentPosn = "1 2 N";
-		final String movements = "LMLMLMLMM";
-		final Rover rover = new Rover(roverCurrentPosn.toUpperCase());
-		final String finalPos = rover.move(movements);
-		System.out.println(finalPos);
-		assertEquals(finalPos, "1 3 N");
-		
-	}*/
+
+public class RoverTest {
+
+	private Rover rover;
 	
-	/**
-	 * This method tests the Rover Movement for the existing inputs given in the mail
-	 * 
-	 * INPUT AND OUTPUT
-	 * Test Input:
-	 * 5 5
-	 * 3 3 E
-	 * MMRMMRMRRM
-	 * 
-	 * Expected Output:
-	 * 5 1 E
-	 * 
-	 */
-	@Test
-	public static void main(String args[]) {
-		Rover rover = new Rover();
-		rover.setPosition(1, 2, N);
-		rover.process("LMLMLMLMM");
-		rover.printPosition(); // prints 1 3 N
-		rover.setPosition(3, 3, E);
-		rover.process("MMRMMRMRRM");
-		rover.printPosition(); // prints 5 1 E
+	@Before
+	public void setUp(){
+		rover = new Rover();
 	}
+	
+	
+	@Test
+	public void TestePosicao(){
+		rover.setPosition(1, 2, 3);
+		assertTrue(rover.x == 1);
+		assertTrue(rover.y == 2);
+		assertTrue(rover.facing == 3);
+				
+		/*BigDecimal menorQue50k = new BigDecimal("25000");
+		conta.deposita(menorQue50k);
+		assertTrue(conta.getCategoria() == Categoria.S);
+		assertEquals(conta.getSaldo(), menorQue50k);
+	}*/
+}
 }
